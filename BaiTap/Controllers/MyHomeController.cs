d21443816be81e580afcc7164dd2b4b17042ad7b
ngothithanhh <ngothanhthcstanhung@gmail.com>
2025-09-04 20:31:13 +0700

    Add project files.

---
+++ BaiTap/Controllers/MyHomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<string>
+            {
+                "Nồi cơm điện cao tần Nagakawa NAG0102",
+                "Nồi cơm điện cao tần Nagakawa NAG0102",
+                "Nồi cơm điện cao tần Nagakawa NAG0102"
+
+            };
+            return View(products);
+        }
+    }
+}
---
+++ BaiTap/ViewComponents/HotProductViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class HotProductViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var hotProducts = new List<string>
+        {
+            "Nồi cơm điện cao tần Nagakawa NAG0102",
+            "Nồi cơm điện cao tần Nagakawa NAG0102",
+            "Nồi cơm điện cao tần Nagakawa NAG0102"
+        };
+
+        return View(hotProducts);
+    }
+}
